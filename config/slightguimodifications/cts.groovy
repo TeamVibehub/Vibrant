c00926dfa9fbde8b85c8a91ec08b73e8a0d1271a
Ruski <max.wagner2003@gmail.com>
2020-09-20 20:40:14 +0100

    CTS Complete, added Plenty mods

--- config/slightguimodifications/cts.groovy
+++ config/slightguimodifications/cts.groovy
@@ -9,17 +9,23 @@ mainMenu {
         splashesEnabled = false
     }
 
-    background {
-        clearBackgrounds()
-        backgroundStayLength = 10000
-        backgroundFadeLength = 1000
-        renderGradientShade = true
-        image { texture = file("config/slightguimodifications/background.png") } // Add Frames to make a Gif background with gaussian blur
-    }
-
     removeMinecraftLogo()
     removeEditionBadge()
     clearAllButtons()
+    clearAllLabels()
+
+    label {
+        position {
+            x = 2
+            y {
+                it - 10
+            }
+        }
+        color = 0x201f3d
+        hoveredColor = 0x19d1a6
+        shadow = true
+        text = literal("Vibrant - Build 0.1.0 (Beta)")
+    }
 
     label {
         position {
@@ -28,32 +34,43 @@ mainMenu {
                 it - 20
             }
         }
-
-        text = literal("Vibrant - Build 0.1.0 (Beta)")
+        color = 0x201f3d
+        hoveredColor = 0x19d1a6
+        shadow = true
+        text = literal("Minecraft Fabric 1.16.3")
     }
+
+    label {
+        position {
+            x { it - 2 }
+            y {
+                it - 10
+            }
+        }
+
+        align = "right"
+        color = 0x201f3d
+        hoveredColor = 0x19d1a6
+        shadow = true
+        text = literal("Copyright Mojang AB. Do not Distribute!")
+    }
+
+   // Title Button
 
     button {
         position {
-            x { it / 2 + 3 }
-            y = 203
+            x { it / 2 }
+            y = 31
         }
-        width = 100
-        height = 20
-
-        text = modMenuText()
-        onClicked = modMenu()
+        width = 300
+        height = 80
+        align = "center"
+        texture = file("config/slightguimodifications/logo.png")
+        onClicked = nothing()
     }
-    button {
-        position {
-            x { it / 2 - 101 }
-            y = 203
-        }
-        width = 100
-        height = 20
 
-        text = literal("Languages")
-        onClicked = language()
-    }
+    //////////// Menu Buttons Start Here ////////////
+
     button {
         position {
             x { it / 2 - 101 }
@@ -62,6 +79,8 @@ mainMenu {
         width = 204
         height = 20
 
+        texture = file("config/slightguimodifications/btn_large.png")
+        hoveredTexture = file("config/slightguimodifications/btn_large-hover.png")
         text = translatable("menu.singleplayer")
         onClicked = singleplayer()
     }
@@ -73,43 +92,12 @@ mainMenu {
         width = 204
         height = 20
 
+        texture = file("config/slightguimodifications/btn_large.png")
+        hoveredTexture = file("config/slightguimodifications/btn_large-hover.png")
         text = translatable("menu.multiplayer")
         onClicked = multiplayer()
     }
-    button {
-        position {
-            x { it / 2 - 101 }
-            y = 251
-        }
-        width = 204
-        height = 20
 
-        text = literal("Reload")
-        onClicked = reloadCts()
-    }
-    
-    button {
-        position {
-            x { it / 2 + 3 }
-            y = 227
-        }
-        width = 100
-        height = 20
-
-        text = translatable("menu.quit")
-        onClicked = exit()
-    }
-    button {
-        position {
-            x { it / 2 - 101 }
-            y = 227
-        }
-        width = 100
-        height = 20
-
-        text = translatable("menu.options")
-        onClicked = options()
-    }
     button {
         position {
             x { it / 2 + 3 }
@@ -118,6 +106,8 @@ mainMenu {
         width = 100
         height = 20
 
+        texture = file("config/slightguimodifications/btn_med.png")
+        hoveredTexture = file("config/slightguimodifications/btn_med-hover.png")
         text = literal("Our Website")
         onClicked = url("https://vibb.in")
     }
@@ -129,7 +119,67 @@ mainMenu {
         width = 100
         height = 20
 
+        texture = file("config/slightguimodifications/btn_med.png")
+        hoveredTexture = file("config/slightguimodifications/btn_med-hover.png")
         text = literal("Discord")
         onClicked = url("https://vibb.in/discord")
     }
+
+    button {
+        position {
+            x { it / 2 + 3 }
+            y = 203
+        }
+        width = 100
+        height = 20
+
+        texture = file("config/slightguimodifications/btn_med.png")
+        hoveredTexture = file("config/slightguimodifications/btn_med-hover.png")
+        text = modMenuText()
+        onClicked = modMenu()
+    }
+    
+    button {
+        position {
+            x { it / 2 - 101 }
+            y = 203
+        }
+        width = 100
+        height = 20
+
+        texture = file("config/slightguimodifications/btn_med.png")
+        hoveredTexture = file("config/slightguimodifications/btn_med-hover.png")
+        text = translatable("menu.options")
+        onClicked = options()
+    }
+
+    button {
+        position {
+            x { it / 2 - 101 }
+            y = 227
+        }
+        width = 204
+        height = 20
+
+        texture = file("config/slightguimodifications/btn_large_exit.png")
+        hoveredTexture = file("config/slightguimodifications/btn_large_exit-hover.png")
+        text = translatable("\u00A74\u00A7lLeave Vibrant")
+        onClicked = exit()
+    }
+
+    // Reload Button
+
+    // button {
+    //     position {
+    //         x { it / 2 - 101 }
+    //         y = 251
+    //     }
+    //     width = 204
+    //     height = 20
+
+    //     texture = file("config/slightguimodifications/btn_large_exit.png")
+    //     hoveredTexture = file("config/slightguimodifications/btn_large_exit-hover.png")
+    //     text = literal("\u00A76\u00A7lReload Custom Title Screen")
+    //     onClicked = reloadCts()
+    // }
 }
